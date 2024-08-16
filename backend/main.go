12990d889d753b1504f999805b14080d6b3e4445
hendisantika <hendisantika@yahoo.co.id>
2024-08-16 07:42:37 +0700

    feat: Add main function 🫸🌀✏️📗🐧🐳

--- backend/main.go
+++ backend/main.go
@@ -2,10 +2,31 @@ package backend
 
 import (
 	"fmt"
+	"github.com/gofiber/fiber/v2"
 	"github.com/spf13/viper"
+	"go-fiber-keycloak-nextjs/api/middlewares"
+	"go-fiber-keycloak-nextjs/api/routes"
 	"log"
 	"strings"
 )
+
+func main() {
+	initViper()
+
+	app := fiber.New(fiber.Config{
+		AppName:      "My demo",
+		ServerHeader: "Fiber",
+	})
+	middlewares.InitFiberMiddlewares(app, routes.InitPublicRoutes, routes.InitProtectedRoutes)
+
+	var listenIp = viper.GetString("ListenIP")
+	var listenPort = viper.GetString("ListenPort")
+
+	log.Printf("will listen on %v:%v", listenIp, listenPort)
+
+	err := app.Listen(fmt.Sprintf("%v:%v", listenIp, listenPort))
+	log.Fatal(err)
+}
 
 func initViper() {
 	viper.SetConfigName("config")

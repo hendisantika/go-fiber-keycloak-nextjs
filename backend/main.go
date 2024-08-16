e02f0848f74df89db0d2f6a0b4a6ffec8be3e60c
hendisantika <hendisantika@yahoo.co.id>
2024-08-16 07:42:04 +0700

    feat: Add initViper 🫸🌀✏️📗🐧🐳

--- backend/main.go
+++ backend/main.go
@@ -1,1 +1,23 @@
 package backend
+
+import (
+	"fmt"
+	"github.com/spf13/viper"
+	"log"
+	"strings"
+)
+
+func initViper() {
+	viper.SetConfigName("config")
+	viper.SetConfigType("json")
+	viper.AddConfigPath(".")
+	viper.SetEnvPrefix("demo")
+	viper.AutomaticEnv()
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(fmt.Errorf("unable to initialize viper: %w", err))
+	}
+	log.Println("viper config initialized")
+}

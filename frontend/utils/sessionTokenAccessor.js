c0b754880634696fd295f338cde0771c6185b821
hendisantika <hendisantika@yahoo.co.id>
2024-08-17 15:12:18 +0700

    feat: Add getIdToken utils 🫸🌀✏️📗🐧🐳

--- frontend/utils/sessionTokenAccessor.js
+++ frontend/utils/sessionTokenAccessor.js
@@ -1,9 +1,21 @@
+import {decrypt} from "./encryption";
+
 export async function getAccessToken() {
 
     const session = await getServerSession(authOptions);
     if (session) {
         const accessTokenDecrypted = decrypt(session.access_token)
         return accessTokenDecrypted;
+    }
+    return null;
+}
+
+export async function getIdToken() {
+
+    const session = await getServerSession(authOptions);
+    if (session) {
+        const idTokenDecrypted = decrypt(session.id_token)
+        return idTokenDecrypted;
     }
     return null;
 }
